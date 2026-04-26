59942228c004f475e16242a191c0d72e142a565d
JhonmaSG <jmserranog@udistrital.edu.co>
2026-04-25 23:48:21 -0500

    chore: created base microservices structure

---
+++ budget-service/budget-service/src/test/java/com/finance/budgetservice/BudgetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.budgetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
